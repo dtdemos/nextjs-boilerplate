bf20e3111cb0b517d48fa43459de8d63b2f3e6d7
Michael Beemer <beeme1mr@users.noreply.github.com>
2023-09-07 12:53:16 -0400

    Remove script tag

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <script type="text/javascript" src="https://js-cdn.dynatrace.com/jstag/157944990f8/bf83471ois/3d03307895552e3e_complete.js" crossorigin="anonymous"></script>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
