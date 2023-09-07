34f425e0744e5b27e39bf2eda2a5ad445a457416
Michael Beemer <beeme1mr@users.noreply.github.com>
2023-09-07 12:57:59 -0400

    Add script tag to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Html, Head } from 'next/document'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +17,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <Html>
+      <Head>
+         <Script
+           src="https://js-cdn.dynatrace.com/jstag/157944990f8/bf83471ois/3d03307895552e3e_complete.js"
+           strategy="beforeInteractive"
+          />
+      </Head>
       <body className={inter.className}>{children}</body>
-    </html>
+    </Html>
   )
 }
