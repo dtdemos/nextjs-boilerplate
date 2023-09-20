e488c170bcb61c015905b9dbf0c3f330628bc35f
Rob Jahn <dtdemos1@gmail.com>
2023-09-20 14:26:32 -0400

    adjust to have a few more pages

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,26 +6,11 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
-          <meta
-            name="description"
-            content="See pictures from Next.js Conf and the After Party."
-          />
-          <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
-          <meta
-            property="og:description"
-            content="See pictures from Next.js Conf and the After Party."
-          />
-          <meta property="og:title" content="Next.js Conf 2022 Pictures" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
-          <meta
-            name="twitter:description"
-            content="See pictures from Next.js Conf and the After Party."
-          />
           <script
             type="text/javascript"
             src="https://js-cdn.dynatrace.com/jstag/157944990f8/bf83471ois/3d03307895552e3e_complete.js"
             crossOrigin="anonymous"
+            async
           ></script>
         </Head>
         <body className="bg-black antialiased">
